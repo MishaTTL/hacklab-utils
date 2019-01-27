9932e000967e69b5c0da9def4f8d5a77e90ade78
MishaTTL <misha.at.ttl@gmail.com>
2019-01-26 23:44:09 -0500

    version 1

---
+++ SQL2LDAP.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+	host="localhost",
+	user="",
+	passwd="",
+	database="memberdb"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT id,cn,givenName,sn,uid,mail FROM ldap_people LIMIT 1")
+
+myresult = mycursor.fetchall()
+
+#Writes the .ldif formatted output
+for r in myresult:
+	print("dn: uid=%s,ou=People,dc=hacklab,dc=to" % r[4])
+	print("cn: %s" % r[1])
+	print("objectClass: top")
+	print("objectClass: person")
+	print("objectClass: organizationalPerson")
+	print("objectClass: inetOrgPerson")
+	print("objectClass: shadowAccount")
+	print("objectClass: posixAccount")
+	print("loginShell: /bin/bash")
+	print("homeDirectory: /home/%s" % r[1])
+	print("gidNumber: 100000")
+	print("sn: %s" % r[3])
+	print("mail: %s" % r[5])
+	print("uid: %s" % r[4])
+	print("uidNumber: %i" % int(r[0])+100000))
+	print("givenName: %s" % r[2])
+	print("")
